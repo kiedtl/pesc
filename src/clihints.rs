e53959ee7b33ab0eb1d5253b098d3db24a3974d3
Kiëd Llaentenn <kiedtl@tilde.team>
2020-10-01 23:12:46 +0000

    prompt: implement hints

---
+++ src/clihints.rs
@@ -0,0 +1,61 @@
+// the following code was proudly stolen
+// from the `diy_hint` example in rustyline's source.
+
+use std::collections::HashSet;
+use crate::pesc::Pesc;
+use rustyline::{
+    hint::Hinter, Context
+};
+use rustyline_derive::{
+    Completer, Helper,
+    Highlighter, Validator
+};
+
+#[derive(Completer, Helper, Validator, Highlighter)]
+pub struct CommandHinter {
+    // TODO: use ** radix trie **
+    hints: HashSet<String>,
+}
+
+impl CommandHinter {
+    pub fn new(hints: HashSet<String>) -> Self {
+        Self { hints: hints }
+    }
+}
+
+impl Hinter for CommandHinter {
+    fn hint(&self, line: &str, pos: usize, _ctx: &Context<'_>) -> Option<String> {
+        if pos < line.len() {
+            return None;
+        }
+
+        self.hints
+            .iter()
+            .filter_map(|hint| {
+                // expect hint after word complete, like redis cli, add condition:
+                // line.ends_with(" ")
+                if pos > 0 && hint.starts_with(&line[..pos]) {
+                    Some(hint[pos..].to_owned())
+                } else {
+                    None
+                }
+            })
+            .next()
+    }
+}
+
+pub fn hints(p: &Pesc) -> HashSet<String> {
+    let mut set = HashSet::new();
+
+    // some hints ;)
+    set.insert(String::from("help // read the manpage, dummy"));
+    set.insert(String::from("quit // just hit Ctrl-D"));
+    set.insert(String::from("exit // it's ^D to quit. Ctrl-D."));
+    set.insert(String::from("man  // `man pesc`, if your package manager's worth anything."));
+    set.insert(String::from("man pesc // no, you need to exit first."));
+
+    p.funcs.iter().for_each(|f| {
+        set.insert(format!("[{}]", f.0));
+    });
+    set
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 mod errors;
 mod pesc;
 mod stdlib;
+mod clihints;
 
 use crate::pesc::*;
-use rustyline::error::ReadlineError;
+use crate::clihints::*;
+
+use rustyline::{
+    config::{
+        Builder,
+        EditMode,
+    },
+    error::ReadlineError,
+    Editor,
+};
 
 fn main() {
     let mut pesc = Pesc::new();
@@ -12,6 +22,8 @@ fn main() {
         pesc.load(func.0, func.1, func.2);
     }
 
+    // waitaminute, let's see if there are args we
+    // can execute
     let args = std::env::args().collect::<Vec<String>>();
     if args.len() > 1 {
         let parsed = match pesc.parse(&args[1]) {
@@ -28,7 +40,16 @@ fn main() {
         }
     }
 
-    let mut rl = rustyline::Editor::<()>::new();
+    // nope, display a pretty prompt & take orders
+    // from stdin
+    let config = Builder::new()
+        .auto_add_history(true)
+        .history_ignore_space(true)
+        .edit_mode(EditMode::Vi)
+        .build();
+
+    let mut rl = Editor::<CommandHinter>::with_config(config);
+    rl.set_helper(Some(CommandHinter::new(hints(&pesc))));
 
     loop {
         match rl.readline("pesc> ") {
