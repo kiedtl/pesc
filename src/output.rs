29f0bc623a52c76ceda757d864c58395bcc9ed51
Kiëd Llaentenn <kiedtl@tilde.team>
2020-10-03 01:23:19 +0000

    output: display Human stack more nicely

--- src/output.rs
+++ src/output.rs
@@ -1,6 +1,8 @@
 use crate::pesc::*;
 use crate::tty::{self, OutputStream};
 
+const PADDING: usize = 11;
+
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum OutputMode {
     Human, Machine, Simple, Quiet
@@ -18,20 +20,31 @@ impl OutputMode {
     pub fn format_stack(&self, p: &Pesc) {
         match self {
             OutputMode::Human => {
-                let max_sz = tty::tty_sz().0;
-                let mut buf = String::new();
-
-                for i in p.m_stack.iter().rev() {
-                    if buf.len() + i.to_string().len() + 1
-                        >= max_sz {
-                            buf = format!("{} »", buf);
-                            break;
-                    }
-
-                    buf = format!("{}{} ", buf, i);
+                if p.m_stack.len() == 0 {
+                    println!("(empty stack)");
+                    return;
                 }
 
-                println!("{}", buf);
+                let max_sz = tty::tty_sz().0;
+                let mut item_buf = String::new();
+                let mut num_buf  = String::new();
+                let mut ctr = 0;
+
+                for i in p.m_stack.iter().rev() {
+                    let item = format!("[{item:>0$}]", PADDING,
+                        item = i.to_string());
+                    if item_buf.len() + item.len() + 1 >= max_sz {
+                        item_buf = format!("{} »", item_buf);
+                        break;
+                    } else {
+                        item_buf += &item;
+                        num_buf  += &format!("{c:>0$}", item.len(),
+                            c = ctr);
+                        ctr += 1;
+                    }
+                }
+
+                println!("{}\n{}", item_buf, num_buf);
             },
             OutputMode::Machine => unimplemented!(),
             OutputMode::Simple
